165c3b848a7e4ea419ee5c1e6204a9a6b7958dec
arepina <anastasiya.repina2012@yandex.ru>
2017-02-15 16:59:55 +0300

    Fixed the words bug

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -18,18 +18,17 @@ public class Main {
 
     public static void main(final String[] args) throws MyStemApplicationException {
         String productName = "соленая домик полки"; //Product name
-        productName = productName.replace(" ", "%20");
-
         String regionsString = Arrays.toString(new Integer[]{12, 57, 77});//Regions list
         String ocpd2CodesString = Arrays.toString(new String[]{"17.12.14.160", "17.12.14.162"});//OCPD2 classificator codes
         //measure = enc2utf8 //measure
 
-        final Iterable<Info> result =
+        Iterable<Info> result =
                 JavaConversions.asJavaIterable(
                         mystemAnalyzer
                                 .analyze(Request.apply(productName))
                                 .info()
                                 .toIterable());
+
         Info firstNoun = null;
         for (final Info info : result) {
             JSONObject jObject  = new JSONObject(info.rawResponse());
@@ -43,6 +42,5 @@ public class Main {
         }
 
         double y = 0.5;
-
     }
 }
