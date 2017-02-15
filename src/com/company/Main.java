20ff628d5b25819394879f8b1ce95e71b43684f2
arepina <anastasiya.repina2012@yandex.ru>
2017-02-15 16:51:16 +0300

    Fixed the method

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,6 @@
 package com.company;
 
+import org.json.JSONObject;
 import ru.stachek66.nlp.mystem.holding.Factory;
 import ru.stachek66.nlp.mystem.holding.MyStem;
 import ru.stachek66.nlp.mystem.holding.MyStemApplicationException;
@@ -16,20 +17,32 @@ public class Main {
     private final static MyStem mystemAnalyzer = new Factory("-igd --eng-gr --format json --weight").newMyStem("3.0", Option.<File>empty()).get();
 
     public static void main(final String[] args) throws MyStemApplicationException {
-        String productName = "соленая"; //Product name
+        String productName = "соленая домик полки"; //Product name
         productName = productName.replace(" ", "%20");
+
         String regionsString = Arrays.toString(new Integer[]{12, 57, 77});//Regions list
         String ocpd2CodesString = Arrays.toString(new String[]{"17.12.14.160", "17.12.14.162"});//OCPD2 classificator codes
         //measure = enc2utf8 //measure
+
         final Iterable<Info> result =
                 JavaConversions.asJavaIterable(
                         mystemAnalyzer
                                 .analyze(Request.apply(productName))
                                 .info()
                                 .toIterable());
-
+        Info firstNoun = null;
         for (final Info info : result) {
+            JSONObject jObject  = new JSONObject(info.rawResponse());
+            String analysis = jObject.get("analysis").toString();
+            analysis = analysis.replace("[", "");
+            analysis = analysis.replace("]", "");
+            String gr = new JSONObject(analysis).get("gr").toString();
+            if (firstNoun == null && gr.charAt(0) == 'S')
+                firstNoun = info;
             System.out.println(info.initial() + " -> " + info.lex() + " | " + info.rawResponse());
         }
+
+        double y = 0.5;
+
     }
 }
