43d28487f6894b590f23f6990236de4ba4c6d04d
arepina <anastasiya.repina2012@yandex.ru>
2017-02-15 17:35:30 +0300

    Uploaded the latest version

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,6 +10,7 @@ import scala.Option;
 import scala.collection.JavaConversions;
 
 import java.io.*;
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Main {
@@ -17,19 +18,21 @@ public class Main {
     private final static MyStem mystemAnalyzer = new Factory("-igd --eng-gr --format json --weight").newMyStem("3.0", Option.<File>empty()).get();
 
     public static void main(final String[] args) throws MyStemApplicationException {
-        String productName = "соленая домик полки"; //Product name
+        String productName = Main.replacer("соленая белый полки"); //Product name
+
         String regionsString = Arrays.toString(new Integer[]{12, 57, 77});//Regions list
         String ocpd2CodesString = Arrays.toString(new String[]{"17.12.14.160", "17.12.14.162"});//OCPD2 classificator codes
         //measure = enc2utf8 //measure
 
         Iterable<Info> result =
                 JavaConversions.asJavaIterable(
-                        mystemAnalyzer
+                                mystemAnalyzer
                                 .analyze(Request.apply(productName))
                                 .info()
                                 .toIterable());
 
         Info firstNoun = null;
+        ArrayList<String> lemmatizedArray = new ArrayList<String>();
         for (final Info info : result) {
             JSONObject jObject  = new JSONObject(info.rawResponse());
             String analysis = jObject.get("analysis").toString();
@@ -39,8 +42,40 @@ public class Main {
             if (firstNoun == null && gr.charAt(0) == 'S')
                 firstNoun = info;
             System.out.println(info.initial() + " -> " + info.lex() + " | " + info.rawResponse());
+            lemmatizedArray.add(info.lex().toString().substring(5, info.lex().toString().length() - 1));
         }
+        System.out.println(firstNoun);
 
         double y = 0.5;
     }
+
+    private static String replacer(String item) {
+        item = item.replaceAll("\"", "");
+        item = item.replaceAll("(\\d)(\\D)", "\\1\\ \\2");
+        item = item.replaceAll("(\\D)(\\d)", "\\1\\ \\2");
+        item = item.replaceAll("(\\D)(\\.|,)(\\D)", "\\1\\ \\3");
+
+        item = item.replace("\r", " ");
+        item = item.replace("\t", " ");
+        item = item.replace("\n", " ");
+        item = item.replace(",", "");
+        item = item.replace(".", "");
+        item = item.replace("•", "");
+        item = item.replace(";", "");
+        item = item.replace(":", "");
+        item = item.replace("!", "");
+        item = item.replace("?", "");
+        item = item.replace(")", "");
+        item = item.replace("(", "");
+        item = item.replace("™", "");
+        item = item.replace("®", "");
+        item = item.replace("*", "");
+        item = item.replace("/", "");
+        item = item.replace("—", "");
+        item = item.replace("-", "");
+        item = item.replace("~", "");
+        item = item.replace("'", "");
+        return item.toLowerCase();
+    }
+
 }
