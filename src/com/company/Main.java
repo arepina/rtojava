c42d32a6a6b608c5ac1769cc3feafda816ea3248
Anastasia Repina <anastasiya.repina2012@yandex.ru>
2017-03-06 22:40:52 +0300

    Improved the main class

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -13,38 +13,51 @@ import java.io.*;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Map;
 
 public class Main {
 
+    private static ArrayList<FileType> dfm;
+    private static ArrayList<String> headers;
+    private static DB db;
+    private static Integer[][] matrix;
     private final static MyStem mystemAnalyzer = new Factory("-igd --eng-gr --format json --weight").newMyStem("3.0", Option.<File>empty()).get();
 
     public static void main(final String[] args) throws MyStemApplicationException, IOException, SQLException, ClassNotFoundException {
+        loadData();
+
+        String request = "set prisma";
+        Info firstNoun = null;
+        ArrayList<String> lemmatizedArray = null;
+
+        processRequest(request, firstNoun, lemmatizedArray);
+
+        workWithDTM(request, firstNoun);
+
+        //Product product = db.getProduct(118583781);
+    }
+
+    private static void loadData() {
         ArrayList<FileType> dfm = new ArrayList<FileType>();
         ArrayList<String> headers = ReadCSV.read("../nmzk/data/dfm.csv", dfm, "dfm");
-        Integer[][] matrix = ReadCSV.formMatrix(dfm);
-        DB db = new DB();
+        matrix = ReadCSV.formMatrix(dfm);
+        db = new DB();
         db.connectDb();
+    }
 
-        String str = "Бумага офисная";
-        str = Product.replacer(str);
-        //Product product = db.getProduct(118583781);
+    private static void processRequest(String request, Info firstNoun, ArrayList<String> lemmatizedArray) throws MyStemApplicationException {
+        request = Product.replacer(request);
 
         Iterable<Info> result =
                 JavaConversions.asJavaIterable(
                         mystemAnalyzer
-                                .analyze(Request.apply(str))
+                                .analyze(Request.apply(request))
                                 .info()
                                 .toIterable());
-
-        ArrayList<String> lemmatizedArray = formLemmatizedArray(result);
-
-        double y = 0.5;
+        lemmatizedArray = formLemmatizedArray(result, firstNoun);
     }
 
-    private static ArrayList<String> formLemmatizedArray(Iterable<Info> result) {
-        Info firstNoun = null;
+    private static ArrayList<String> formLemmatizedArray(Iterable<Info> result, Info firstNoun) {
         ArrayList<String> lemmatizedArray = new ArrayList<String>();
         for (final Info info : result) {
             JSONObject jObject = new JSONObject(info.rawResponse());
@@ -61,6 +74,31 @@ public class Main {
         return lemmatizedArray;
     }
 
+    private static void workWithDTM(String request, Info firstNoun) {
+        ArrayList<Integer> nonZeroRows = findNonZeroRows(request, headers, matrix);
+        if (nonZeroRows.size() == 0) // we don't have a row or more with all the words
+        {
+            try {
+                ArrayList<Integer> nonZeroFirstNounRows = findFirstNounRows(firstNoun.toString(), headers, matrix);
+                calculateCos(nonZeroFirstNounRows, 0.7);
+            } catch (NullPointerException e) {
+                System.err.println("Didn't find the first noun in request");
+                //todo find the first slovarniy term!!!
+                //if not found return new ArrayList<Product>();
+                //else calculateCos(nonZeroFirstTermRows, 0.7);
+            }
+
+        }
+        else{
+            calculateCos(nonZeroRows, 0.5);
+        }
+    }
+
+    private static void calculateCos(ArrayList<Integer> nonZeroRows, double y) {
+
+    }
+
+
     private static ArrayList<Integer> findNonZeroRows(String str, ArrayList<String> headers, Integer[][] matrix) {
         String[] words = str.split(" ");
         Map<String, Integer> columnWords = new HashMap<String, Integer>(); // column for each word
@@ -71,7 +109,7 @@ public class Main {
                 int columnNumber = headers.indexOf(word);
                 columnWords.put(word, columnNumber);
             } else
-                throw new IllegalArgumentException("Unknown word");
+                throw new IllegalArgumentException("Unknown word in non zero rows");
         }
         Map<String, ArrayList<Integer>> rowsWords = new HashMap<String, ArrayList<Integer>>(); // non zero rows for each word column
         for (Object o : columnWords.entrySet()) {// get the non zero rows for each word column
@@ -81,11 +119,11 @@ public class Main {
             ArrayList<Integer> rows = new ArrayList<Integer>();
             if (rowsWords.get(pair.getKey().toString()) != null)
                 rows = rowsWords.get(pair.getKey().toString());
-            for(int i = 0; i < matrix.length; i++)
-            {
-                if (matrix[i][columnIndex] != 0)
+            for (int i = 0; i < matrix.length; i++) {
+                if (matrix[i][columnIndex - 1] != 0)
                     rows.add(i);
             }
+            rowsWords.put(pair.getKey().toString(), rows);
         }
         Map<Integer, Integer> rowsNumber = new HashMap<Integer, Integer>(); // number of each non zero row
         for (ArrayList<Integer> o : rowsWords.values()) {
@@ -98,12 +136,25 @@ public class Main {
             }
         }
         ArrayList<Integer> resultRows = new ArrayList<Integer>();
-        for(Integer rowNumber: rowsNumber.keySet())
-        {
+        for (Integer rowNumber : rowsNumber.keySet()) {
             if (rowsNumber.get(rowNumber) == columnWords.keySet().size())
                 resultRows.add(rowNumber);
         }
         return resultRows;
+    }
+
+    private static ArrayList<Integer> findFirstNounRows(String firstNoun, ArrayList<String> headers, Integer[][] matrix) {
+        int columnIndex = 0;
+        if (headers.contains(firstNoun))
+            columnIndex = headers.indexOf(firstNoun);
+        else
+            throw new IllegalArgumentException("Unknown first noun word");
+        ArrayList<Integer> rows = new ArrayList<Integer>(); // non zero rows for word column
+        for (int i = 0; i < matrix.length; i++) {
+            if (matrix[i][columnIndex - 1] != 0)
+                rows.add(i);
+        }
+        return rows;
     }
 
 }
